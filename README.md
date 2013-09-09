7f2bcef1fc295f1c6d904425918c2e854cb5ba2f
John Romkey <romkey@romkey.com>
2013-09-09 16:53:53 -0700

    restructuring to better support building both Chrome and Safari extensions
    work on gruntfile to copy files around appropriately
    
    more icons

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 ShowReferrer
 ============
 
-Chrome Extension that adds markup a web page, showing the referrer to it
+Browser Extension that adds markup a web page, showing the referrer to it
 
-The extension's web page is http://romkey.com/code/show-referrer-chrome-extension/
+The extension's web page is http://romkey.com/code/show-referrer-browser-extension/
 
-Download the extension the Chrome web store at https://chrome.google.com/webstore/detail/cnobdfgbihnhbkiefjohgkjlobpjfpam
+Download the Chrome extension from the Chrome web store at https://chrome.google.com/webstore/detail/cnobdfgbihnhbkiefjohgkjlobpjfpam
+
+Download the Safari extension from https://www.dropbox.com/s/jhdvgm994kitm9c/ShowReferrer.safariextz
+
+
+Tasks
+=====
+
+1. Bump version number
+
+grunt bumpup:[major|minor|patch]
+
+2. Build Chrome distribution
+
+grunt dist
+
--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,11 @@ module.exports = function( grunt ) {
     grunt.loadNpmTasks( "grunt-git-authors" );
     grunt.loadNpmTasks( "grunt-contrib-jshint" );
     grunt.loadNpmTasks( "grunt-contrib-compress" );
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-contrib-copy" );
+    grunt.loadNpmTasks( "grunt-contrib-imagemin" );
+    grunt.loadNpmTasks( "grunt-text-replace" );
+    grunt.loadNpmTasks( "grunt-version" );
 
     grunt.initConfig( {
 	jshint: {
@@ -22,8 +27,44 @@ module.exports = function( grunt ) {
 		]
 	    }
 	},
+	copy: {
+	    main: {
+		files: [
+		    { expand: true, cwd: './lib/js', src: [ '*' ], dest:  'lib/ShowReferrer.crx/js/' },
+		    { expand: true, cwd: './lib/js', src: [ '*' ], dest:  'lib/ShowReferrer.safariextension/js/' },
+
+		    { expand: true, cwd: './lib/img', src: [ '*' ], dest: 'lib/ShowReferrer.crx/img/' },
+		    { expand: true, cwd: './lib/img', src: [ '*' ], dest: 'lib/ShowReferrer.safariextension/img/' },
+		]
+	    }
+	},
+	watch: {
+	    scriptsandcss: {
+		files: [ 'lib/js/**/*.js' ],
+		tasks: [ 'copy' ],
+		options: {
+		    nospawn: true
+		}
+	    },
+	    images: {
+		files: [ 'lib/img/**/*.jpg', 'lib/img/**/*.png', 'lib/img/**/*.gif' ],
+		tasks: [ 'imagemin' ],
+		options: {
+		    nospawn: true
+		}
+	    }
+	},
+	version: {
+	    pkg: grunt.file.readJSON( 'package.json' )
+	},
+	replace: {
+	    version: {
+		src: [ 'lib/ShowReferrer.crx/manifest.json', 'lib/ShowRefererrer.safariextension/Info.plist' ],
+		overwrite: true,
+	    }
+	}
     } );
 
     grunt.registerTask( 'test', [ 'jshint' ] );
-    grunt.registerTask( 'dist', [ 'compress' ] );
+    grunt.registerTask( 'dist', [ 'text-replace', 'copy', 'compress' ] );
 };
